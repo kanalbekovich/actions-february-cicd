97f405081ece728c0c1806b13735799f6e33cacc
kanalbekovich <kanalbekovich@gmail.com>
2025-06-20 00:09:12 +0000

    new version

--- test_app.py
+++ test_app.py
@@ -12,4 +12,4 @@ def client():
 def test_hello_world(client):
    response = client.get('/')
    assert response.status_code == 200
-   assert response.data == b'Hello, World!'
+   assert response.data == b'Hello, World! NEW VERSION'
